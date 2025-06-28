c0f31d86d7bc88fa56dcf63f11f64dd741fe9127
Victor <victor.meyo@student.moringaschool.com>
2025-06-28 05:51:37 +0300

    Additional notes added

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,35 +1,38 @@
-#!/usr/bin/env python3
+# 1. Functions that checks username and password.
 
 def admin_login(username, password):
     # your code here
     if (username == "admin" or username == "ADMIN") and password == "12345":
-        return "Access granted"
+        # Checks for username 'admin'/'ADMIN' and password '12345'
+        return "Access granted" # If condition is true
     else:
-        return "Access denied"
+        return "Access denied" # If condition is false
     
+# 2. Function that checks weather temperature.
 
 def hows_the_weather(temperature):
     # your code here
     if temperature < 40:
-        return "It's brisk out there!"
+        return "It's brisk out there!" # Return value for temperatures below 40
     elif 40 <= temperature <= 65:
-        return "It's a little chilly out there!"
+        return "It's a little chilly out there!" # Return value for temperatures between 40 and 65
     elif temperature > 85:
-        return "It's too dang hot out there!"
+        return "It's too dang hot out there!" # Return value for temperatures above 85
     else:
         return "It's perfect out there!"
-    
 
-def fizzbuzz(num):
+# 3. Function that checks on multiples of 3 & 5     
+
+def fizzbuzz(num): # Quotient operator used to check on the remainder
     # your code here
     if num % 3 == 0 and num % 5 == 0:
-        return "FizzBuzz"
+        return "FizzBuzz" # Return value for multiples of both 3 & 5
     elif num % 3 == 0:
-        return "Fizz"
+        return "Fizz" # Return value for multiples of 3
     elif num % 5 == 0:
-        return "Buzz"
+        return "Buzz" # Return value for multiples of 5
     else:
-        return num
+        return num # Return value for any other number
     
 
 def calculator(operation, num1, num2):
